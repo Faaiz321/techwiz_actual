d10a54492592610e53cbf0afe80f4fce5041f46c
Hsk4 <hsk42116@gmail.com>
2024-09-20 14:36:10 +0500

    logout added

--- users/index.php
+++ users/index.php
@@ -626,8 +626,9 @@
             <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
               class="material-icons-outlined">cloud_download</i>Downloads</a>
           <hr class="dropdown-divider">
-          <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="javascript:;"><i
-          class="material-icons-outlined">power_settings_new</i>Logout</a>
+          <a class="dropdown-item d-flex align-items-center gap-2 py-2" href="../auth-logout.php">
+             <i class="material-icons-outlined">power_settings_new</i>Logout
+            </a>
         </div>
       </li>
     </ul>
